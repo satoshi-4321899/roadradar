2fe78329920b0beedb8ca9dba8a470c7dfb54ef0
安井悟史 <rafa.satosh1216.gmail.com>
2024-10-03 17:30:04 +0900

    free/product fix3

--- resources/views/free/product/show.blade.php
+++ resources/views/free/product/show.blade.php
@@ -18,9 +18,9 @@
     <div class="max-w-7xl mx-auto px-4 py-5">
         <div class="sm:px-20 mt-4">
             <div class="bg-white w-full rounded-2xl sm:px-10 sm:py-8 shadow-lg hover:shadow-2xl transition duration-500">
-                <div class="text-left sm:flex sm:justify-around">
+                <div class="text-left sm:flex sm:justify-between">
                     <div>
-                        <img src="/storage/images/products/{{ $product->image }}"  class="rounded-md sm:w-96 sm:h-80 max-sm:w-60 max-sm:h-52 object-cover max-sm:mx-auto">
+                        <img src="/storage/images/products/{{ $product->image }}"  class="rounded-md sm:w-1/2 sm:h-80 max-sm:w-60 max-sm:h-52 object-cover max-sm:mx-auto">
                     </div>
                     <div class="flex flex-col p-3">
                         <h2 class="sm:text-xl text-gray-700 font-semibold">
