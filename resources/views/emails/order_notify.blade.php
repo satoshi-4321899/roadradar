54a56b034b26982cac4fe9e326df8a8b82969ff8
安井悟史 <rafa.satosh1216.gmail.com>
2024-10-03 19:34:15 +0900

    emails/order fix2

--- resources/views/emails/order_notify.blade.php
+++ resources/views/emails/order_notify.blade.php
@@ -13,27 +13,19 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
 <body>
-    <h1 class="mt-5 mx-5 text-xl text-red-700 font-semibold">{{ $name }}様</h1>
-    <p class="mt-2 mx-5 text-lg font-semibold">ご購入いただいた商品</p>
-    <ul class="mt-3 mx-5">
-    @foreach ($cartProducts as $product)
-        <li class="flex mb-4">
-            <div>
-                <img src="{{ asset('storage/images/products/' . $product->image) }}" alt="product_image" class="w-60 h-36 object-cover rounded-md">
-            </div>
-            <div class="px-5">
-                <p>{{ $product->name }}</p>
-                <p>¥{{ $product->price }}</p>
-                <p>数量：{{ $product->pivot->quantity }}</p>
-                <p>金額：{{ $product->pivot->amount }}円</p>
-            </div>
-        </li>
-        <div class="flex justify-center">
-            <hr class="border mb-4 w-full">
-        </div>
+    <h1>{{ $name }}様</h1>
+    <p>ご購入いただいた商品</p>
+    @foreach ($cartProducts as $product)  
+        <div class="px-5">
+            <p>{{ $product->name }}</p>
+            <p>¥{{ $product->price }}</p>
+            <p>数量：{{ $product->pivot->quantity }}</p>
+            <p>金額：{{ $product->pivot->amount }}円</p>
+        </div>  
+        <hr>
     @endforeach
     </ul>
-    <div class="flex flex-col items-end pr-3">
+    <div>
         <p>合計金額：<strong>{{ $totalAmount }}円</strong></p>
         <p>お支払い方法：{{ $payment_method }}</p>
         <p>お届け先：{{ $address }}</p>
